c509cb50e27b3ce3e077903a434029a27838211c
Timofeev <ivant87@mail.ru>
2019-10-25 15:14:14 +0300

    создал функцию, для создания объекта

--- js/main.js
+++ js/main.js
@@ -8,30 +8,32 @@ var TIME_CHECKOUT = ['12:00', '13:00', '14:00'];
 var TYPES_FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 var ADDRESS_IMAGES = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
-var getSameHotel = function(xx, title, x, y, price, rooms, guests, description) {
 
+var getSameHotel = function(idx) {
+  var x = getRandomNumb(0, 100);
+  var y = getRandomNumb(101, 200)
   return {
     author: {
-      avatar: "img/avatars/user{{xx}}.png"
+      avatar: "img/avatars/user0" + idx + ".png",
     },
 
     offer: {
-      title,
-      address: "{{location.x}}, {{location.y}}",
-      price,
-      type: TYPES_HOTEL[randomNumb(0, 3)],
-      rooms,
-      guests,
-      checkin: TIME_CHECKIN[randomNumb(0, 3)],
-      checkout: TIME_CHECKOUT[randomNumb(0, 3)],
-      features: TYPES_FEATURES[randomNumb(0, 5)],
-      description,
-      photos: ADDRESS_IMAGES[randomNumb(0, 2)]
+      title: "best room",
+      address: x + "," + y,
+      price: 1000,
+      type: TYPES_HOTEL[getRandomNumb(0, 3)],
+      rooms: 3,
+      guests: 2,
+      checkin: TIME_CHECKIN[getRandomNumb(0, 3)],
+      checkout: TIME_CHECKOUT[getRandomNumb(0, 3)],
+      features: TYPES_FEATURES[getRandomNumb(0, 5)],
+      description: "description room",
+      photos: ADDRESS_IMAGES[getRandomNumb(0, 2)],
     },
 
     location: {
-      x,
-      y: getRandomNumb(130, 630)
+      x: x,
+      y: y,
     }
   }
 };

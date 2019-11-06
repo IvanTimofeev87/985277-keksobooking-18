70f2d1cc7eaa262063b77ef0e6e8359be2a8c90e
Timofeev <ivant87@mail.ru>
2019-11-06 08:33:58 +0300

    сделал задание 5

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
+//Константы
 var TYPES_HOTEL = ['palace', 'flat', 'house', 'bungalo'];
 var TIME_CHECKIN = ['12:00', '13:00', '14:00'];
 var TIME_CHECKOUT = ['12:00', '13:00', '14:00'];
 var TYPES_FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 var ADDRESS_IMAGES = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-var SAME_HOTEL_ARRAY = makeSameHotelArray();
+var SAME_HOTELS = makeSameHotelArray();
 
+//Функции
 function getRandomNumb(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
 function getSameHotel(idx) {
-  var x = getRandomNumb(0, 100);
-  var y = getRandomNumb(101, 200)
+  var x = getRandomNumb(100, 200);
+  var y = getRandomNumb(201, 300);
   return {
     author: {
       avatar: "img/avatars/user0" + idx + ".png",
@@ -40,24 +42,25 @@ function getSameHotel(idx) {
 
 function makeSameHotelArray() {
   var array = [];
-  for (var i = 0; i < 8; i++) {
+  for (var i = 1; i < 9; i++) {
     array.push(getSameHotel(i));
   }
   return array;
 };
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var Map = document.querySelector('.map');
+Map.classList.remove('map--faded');
 
-var similarHotelTemplate = document.querySelector('#pin')
+var SimilarHotelTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 
-for (var i = 0; i < 8; i++) {
-  var hotelElement = similarHotelTemplate.cloneNode(true);
+SAME_HOTELS.forEach(function(item, i) {
+  var hotelElement = SimilarHotelTemplate.cloneNode(true);
   var avatarImage = hotelElement.getElementsByTagName("img");
-  hotelElement.style.left = SAME_HOTEL_ARRAY[i].location.x + 'px';
-  hotelElement.style.top = SAME_HOTEL_ARRAY[i].location.y + 'px';
-  avatarImage[0].src = SAME_HOTEL_ARRAY[i].author.avatar;
-  map.appendChild(hotelElement);
-}
+  hotelElement.style.left = ((SAME_HOTELS[i].location.x) - 25) + 'px';
+  hotelElement.style.top = ((SAME_HOTELS[i].location.y) - 70) + 'px';
+  avatarImage[0].src = SAME_HOTELS[i].author.avatar;
+  avatarImage[0].alt = SAME_HOTELS[i].offer.title;
+  Map.appendChild(hotelElement);
+});

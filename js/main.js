bf0a2b35381f4c920467d52954955b94b6f339a7
Timofeev <ivant87@mail.ru>
2019-11-08 11:31:44 +0300

    сделал задание 5+

--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ var TIME_CHECKOUT = ['12:00', '13:00', '14:00'];
 var TYPES_FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 var ADDRESS_IMAGES = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 var SAME_HOTELS = makeSameHotelArray();
+var PIN_COORDINATE_X = ['100', '200'];
+var PIN_COORDINATE_Y = ['201', '300'];
 
 //Функции
 function getRandomNumb(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+function getRandomItem(array) {
+  i = getRandomNumb(0, array.length - 1);
+  return array[i];
+};
+
 function getSameHotel(idx) {
-  var x = getRandomNumb(100, 200);
-  var y = getRandomNumb(201, 300);
+  var x = getRandomItem(PIN_COORDINATE_X);
+  var y = getRandomItem(PIN_COORDINATE_Y);
+
+
   return {
     author: {
       avatar: "img/avatars/user0" + idx + ".png",
@@ -23,14 +32,14 @@ function getSameHotel(idx) {
       title: "best room",
       address: x + "," + y,
       price: 1000,
-      type: TYPES_HOTEL[getRandomNumb(0, 3)],
+      type: getRandomItem(TYPES_HOTEL),
       rooms: 3,
       guests: 2,
-      checkin: TIME_CHECKIN[getRandomNumb(0, 3)],
-      checkout: TIME_CHECKOUT[getRandomNumb(0, 3)],
-      features: TYPES_FEATURES[getRandomNumb(0, 5)],
+      checkin: getRandomItem(TIME_CHECKIN),
+      checkout: getRandomItem(TIME_CHECKOUT),
+      features: getRandomItem(TYPES_FEATURES),
       description: "description room",
-      photos: ADDRESS_IMAGES[getRandomNumb(0, 2)],
+      photos: getRandomItem(ADDRESS_IMAGES),
     },
 
     location: {
